4e42bdc42dcbda47f8148f17bc4660604c7e376f
Jewel Mia <jewel.mia@technonext.com>
2025-08-26 15:48:02 +0600

    category controller update

--- controllers/categoryController.go
+++ controllers/categoryController.go
@@ -27,6 +27,34 @@ func GetCategories(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(categories)
 }
 
+func getCats(w http.ResponseWriter, r *http.Request){
+	rows, err := database.DB.Query("SELECT id, name from categories")
+	if err != nil {
+		http.Error(w, "DB Error", http.StatusInternalServerError)
+		return
+	}
+defer rows.Close()
+
+	var catObject []models.Category
+	for rows.Next(){
+		var c models.Category
+		if error :=rows.Scan(&c.ID, &c.Name); error != nil {
+			http.Error(w, "Scan error", http.StatusInternalServerError)
+			return
+		}
+		catObject = append(catObject, c)
+	}
+	if err := rows.Err(); err != nil{
+		http.Error(w, "rows Iteration Errorserver error", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type","application/json")
+  if error := json.NewEncoder(w).Encode(catObject); error != nil {
+	http.Error(w, "jsonEncode Error", http.StatusInternalServerError)
+	return
+  }
+}
+
 func AddCategory(w http.ResponseWriter, r *http.Request) {
 
 	var c models.Category
