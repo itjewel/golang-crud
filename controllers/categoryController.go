e35ff902957d71f67f6b5c2c786e92f9580e3ea3
md.jewelmia <md.jewelmia52@gmail.com>
2025-08-27 09:42:58 +0600

    delete update

--- controllers/categoryController.go
+++ controllers/categoryController.go
@@ -3,7 +3,9 @@ package controllers
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"strconv"
 
 	"golang-crud/database"
 	"golang-crud/models"
@@ -60,6 +62,39 @@ func UpateCategory(w http.ResponseWriter, r *http.Request) {
 	}
 	w.Header().Set("Content-type", "application/json")
 	json.NewEncoder(w).Encode(resMessage)
+}
+
+func DeleteCategory(w http.ResponseWriter, r *http.Request) {
+	idStr := r.URL.Query().Get("catId") // string
+	if idStr == "" {
+		http.Error(w, "Missing catId", http.StatusBadRequest)
+		return
+	}
+
+	id, err := strconv.Atoi(idStr) // string -> int conversion
+	if err != nil {
+		http.Error(w, "Invalid catId", http.StatusBadRequest)
+		return
+	}
+	result, err := database.DB.Exec("DELETE FROM categories WHERE id = ? ", id)
+	if err != nil {
+		log.Println("sql error", err)
+		http.Error(w, "sql Error", http.StatusInternalServerError)
+		return
+	}
+	rowErrectFields, _ := result.RowsAffected()
+	if rowErrectFields == 0 {
+		http.Error(w, "No fild updatede", http.StatusNotFound)
+		return
+	}
+	successMessage := models.Response{
+		Message: "Success Delete",
+		Status:  200,
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(successMessage)
+
+	// log.Println("Error", id)
 }
 
 func AddCategory(w http.ResponseWriter, r *http.Request) {
--- routes/category.go
+++ routes/category.go
@@ -10,4 +10,5 @@ func CategoryRoutes(mux *http.ServeMux) {
 	mux.HandleFunc("GET /categories", controllers.GetCategories)        // GET
 	mux.HandleFunc("POST /categories/add", controllers.AddCategory)     // POST
 	mux.HandleFunc("PUT /categories/update", controllers.UpateCategory) // PUT
+	mux.HandleFunc("DELETE /categories/delete", controllers.DeleteCategory)
 }
